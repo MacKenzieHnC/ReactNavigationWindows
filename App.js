4ec823ed006e9dd07383e9181ac0566624d5618b
mackenzie <mackenzie@yourmoms>
2023-01-10 20:09:47 -0600

    Demonstrate bug

--- App.js
+++ App.js
@@ -6,18 +6,39 @@
  * @flow strict-local
  */
 import 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import {View, Text, Button} from 'react-native';
+
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import {SafeAreaView, ScrollView, View} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+const SomeScreen = ({route, navigation}) => {
+  const [someValue, setSomeValue] = useState(route.params.initialValue);
+
+  const incrementSomeValue = () => {
+    setSomeValue(someValue + 1);
+  };
+
+  return (
+    <View style={{alignItems: 'flex-end'}}>
+      <Text style={{fontSize: 30}}>{someValue}</Text>
+      <Button title={'Increment'} onPress={incrementSomeValue} />
+    </View>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View />
-        </ScrollView>
-      </SafeAreaView>
+      <Drawer.Navigator>
+        <Drawer.Screen
+          name="Some Screen"
+          component={SomeScreen}
+          initialParams={{initialValue: 1}}
+        />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
